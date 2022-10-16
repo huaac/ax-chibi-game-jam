44d9e566849af0db7cdfe9252cb6a9987fde849b
huaac <huaac1@uci.edu>
2022-10-16 14:57:11 -0700

    changed the way the game loads the next scene
    used invoke instead

--- AX Chibi Game/Assets/Scripts/LoadNextScene.cs
+++ AX Chibi Game/Assets/Scripts/LoadNextScene.cs
@@ -5,18 +5,34 @@ using UnityEngine.SceneManagement;
  
 public class LoadNextScene : MonoBehaviour
 {
-    public string sceneName;
+    public string nextSceneName;
+    public int seconds;
+    private string currScene;
 
-    void OnEnable()
+    // void OnEnable()
+    // {
+    //     // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+    //     SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    // }
+    void Start()
     {
-        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        currScene = SceneManager.GetActiveScene().name;
+        //SceneManager.LoadScene(sceneName, 115);
+        if(currScene != "Menu Screen")
+        {
+            Invoke("LoadScene", seconds);
+        }
     }
 
-    public void LoadScene(string scene_name)
+    public void LoadScene()
     {
-        SceneManager.LoadScene(scene_name);
+        SceneManager.LoadScene(nextSceneName);
     }
+
+    // public void LoadScene(string scene_name)
+    // {
+    //     SceneManager.LoadScene(scene_name);
+    // }
 
     public void QuitGame()
     {

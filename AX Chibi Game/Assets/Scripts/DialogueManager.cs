b3e08bbe6edf56b44a4ecd76fd0f64841b64c294
AngelRR01 <rubioar@uci.edu>
2022-10-17 00:41:42 -0700

    dl now works and one at a time

--- AX Chibi Game/Assets/Scripts/DialogueManager.cs
+++ AX Chibi Game/Assets/Scripts/DialogueManager.cs
@@ -14,41 +14,34 @@ public class DialogueManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        switch (Time.timeSinceLevelLoad)
+        switch ((int) Time.timeSinceLevelLoad)
         {
             case 30:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 60:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 80:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 85:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 90:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 120:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
             case 150:
                 quips[startIndex].active = true;
-                startIndex++;
                 StartCoroutine(TextDuration());
                 break;
         }
@@ -58,12 +51,12 @@ public class DialogueManager : MonoBehaviour
     public void defeated()
     {
         quips[7].active = true;
-        StartCoroutine(TextDuration());
     }
 
     private IEnumerator TextDuration()
     {
         yield return new WaitForSeconds(3f);
-        quips[startIndex-1].active = false;
+        quips[startIndex].active = false;
+        startIndex++;
     }
 }
